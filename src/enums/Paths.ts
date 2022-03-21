a7e5b7f3f8113c3afc232bb47a56431e5c8a45ee
zabtani <zabtani@gmail.com>
2022-03-21 14:58:17 +0200

    initial commit

--- src/enums/Paths.ts
+++ src/enums/Paths.ts
@@ -1,6 +1,6 @@
 enum Paths {
-  WEATHER = '/weather',
-  FAVORITES = '/favorites',
+  WEATHER = 'Omer-Zabtani-21-03-2022/weather',
+  FAVORITES = 'Omer-Zabtani-21-03-2022/favorites',
   HOME = '/Omer-Zabtani-21-03-2022',
 }
 
