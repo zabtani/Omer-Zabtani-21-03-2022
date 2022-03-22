16e430cd2096c4e5e1c3a0429b6e2933b6d0aeb2
zabtani <zabtani@gmail.com>
2022-03-22 04:43:58 +0200

    f

--- src/enums/Paths.ts
+++ src/enums/Paths.ts
@@ -1,8 +1,8 @@
 enum Paths {
   BASE = '/',
-  HOME = '/Omer-Zabtani-21-03-2022',
-  WEATHER = '/Omer-Zabtani-21-03-2022/weather',
-  FAVORITES = '/Omer-Zabtani-21-03-2022/favorites',
+  HOME = '/',
+  WEATHER = '/weather',
+  FAVORITES = '/favorites',
 }
 
 export default Paths;
