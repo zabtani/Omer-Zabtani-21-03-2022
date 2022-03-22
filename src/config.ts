b0c5805055a70d4e45b77916cffde227ee264454
zabtani <zabtani@gmail.com>
2022-03-22 05:22:37 +0200

    new api key

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
-export const API_KEY00000 = 'apikey=vH5gvP7cP2GfVLfhGRlmSRHgBgLd5MSz';
-export const API_KEY000000 = 'apikey=eqLIHYZfI9z36GQcBAPbFiGWK0U2lScE';
-export const API_KEY0000000 = 'apikey=fmxfIJsMhNnDWQGkWam6FULklYKmP4oe';
-export const API_KEY0000 = 'apikey=g8dTkntA5pSAQHpjaWoHhLmw9S4M2zW5';
-export const API_KEY = 'apikey=r9ognkpAohFVYvuWxKetKeQ3SfGAbGDu';
-
 export const API_KEY00000000 = 'apikey=SA8YyouCOSMqtXe8xrmboGdZ6Sfyar74';
-export const API_KEYDEP = 'apikey=oGm6vRrxw8C2nn8osPTIhpEQJLvL0Q81';
-
+export const API_KEY000000000 = 'apikey=r9ognkpAohFVYvuWxKetKeQ3SfGAbGDu';
+export const API_KEY0000000 = 'apikey=fmxfIJsMhNnDWQGkWam6FULklYKmP4oe';
+export const API_KEY000000 = 'apikey=eqLIHYZfI9z36GQcBAPbFiGWK0U2lScE';
+export const API_KEY00000 = 'apikey=vH5gvP7cP2GfVLfhGRlmSRHgBgLd5MSz';
+export const API_KEY0000 = 'apikey=g8dTkntA5pSAQHpjaWoHhLmw9S4M2zW5';
+export const API_KEY = 'apikey=oGm6vRrxw8C2nn8osPTIhpEQJLvL0Q81';
 export const UNSPLASH_API_KEY = '0oJhi_V8eHNlNtyOLjykytXjPWxFd79JZ7FWUduq0vk';
